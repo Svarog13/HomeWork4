b4fb76208f72c402a57b44dbb5e19a2052508e7c
lenovo <lenovo@DESKTOP-D0T6Q1U>
2023-10-09 22:38:45 +0300

    Add project files.

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Entered 6 digit number: ");
+int digit = int.Parse(Console.ReadLine());
+
+if (digit < 100000 || digit > 999999)
+{
+    Console.WriteLine("Error, you entered wrong digit");
+}
+else
+{
+    string reversedDigit = new string(digit.ToString().Reverse().ToArray());
+    Console.WriteLine($"Hello motherfacker, we have new digit: {reversedDigit}");
+}
